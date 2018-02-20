86db775bba4282578e370606740f0bc44e4fc96c
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-20 19:02:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    count = 0
+    while count < array.length
+      yield(array[count])
+      count += 1
+    end
+    array
+  end
 end
