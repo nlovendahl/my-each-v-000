714fbe596b69d28dea918db44b9674ae70ae328b
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-20 18:55:14 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,13 @@
 def my_each(array)
   count = 0
   while count < array.length
-    yield(array[count]) {|element| puts element}
+    yield(array[count])
   end
   array
+end
+
+def my_each(array)
+  array.each do |element|
+    puts element
+  end
 end
